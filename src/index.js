5dd0cb138918e725ac6fee4b7b50816d418f70e9
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-04 09:59:11 +0300

    refactor : edit folders structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
 const pizzaData = [
@@ -143,12 +141,3 @@ function Pizza({pizzaObj}){
   <React.StrictMode>
       <App/>
   </React.StrictMode>);  
-  reportWebVitals();
-  
-  
-  
-  
-  
-  
-  
-  
